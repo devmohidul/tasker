71e0e73a5ff4a48775f423197d226839b36b875b
devmohidul <mohidulhasan145@gmail.com>
2025-07-02 16:46:31 +0600

    Header updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import Header from "./Header";
+
 export default function App() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-center text-2xl font-bold text-green-400">
-        Welcome to Tailwind Version 4
-      </h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
