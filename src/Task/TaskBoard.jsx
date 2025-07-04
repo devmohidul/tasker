732c009755eeb1b8dfead6268e3a3a382cd7ca3c
devmohidul <mohidulhasan145@gmail.com>
2025-07-05 05:45:34 +0600

    tasklist dynamically comes from taskboards state

--- src/Task/TaskBoard.jsx
+++ src/Task/TaskBoard.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskLists from "./TaskLists";
 
 export default function TaskBoard() {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Learn React",
+    description:
+      "I want to learn react such that i can treat react as my favourite part.",
+    tags: ["web", "react", "js"],
+    priority: "High",
+    isFavorite: true,
+  };
+  const [tasks, setTasks] = useState([defaultTask]);
   return (
     <>
       <section className="mb-20 px-6" id="tasks">
@@ -12,7 +23,7 @@ export default function TaskBoard() {
           </div>
           <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
             <TaskActions />
-            <TaskLists />
+            <TaskLists tasks={tasks} />
           </div>
         </div>
       </section>
